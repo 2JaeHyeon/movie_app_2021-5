d003da419c4b93c7415d9d7c599aafaecd64cdf2
JaeHyeon2 <atom4545@naver.com>
2021-11-03 16:49:35 +0900

    1103.1

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./About.css";
+
+function About() {
+  return (
+
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
+}
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+class Detail extends React.Component{
+    componentDidMount(){
+        const { location }=this.props
+    }
+    render(){
+        const { location }=this.props
+
+        
+        if(location.state.title){
+                
+        }
+    }
+}
+
+export default Detail
