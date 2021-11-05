2f196a20928968c3195eba6ef627837a9dd7636a
JaeHyeon2 <atom4545@naver.com>
2021-11-05 10:43:20 +0900

    1105.1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,22 @@
-import React from 'react';
-
-
+import React from "react";
 
 class Detail extends React.Component{
     componentDidMount(){
-        const { location }=this.props
+        const { location, history} = this.props
+        if(location.state === undefined){
+            history.push("/")
+        }
     }
     render(){
-        const { location }=this.props
-
-        
-        if(location.state.title){
-                
+        const { location } = this.props
+        if(location.state){
+            return(
+                <span>{location.state.title}</span>
+            )
+        } else{
+            return null
         }
+       
     }
 }
 
